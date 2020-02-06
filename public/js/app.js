193f2bf36eb366bae16793049100fcd973be78cc
vijender ahlawat <vijender.x.ahlawat.-nd@disney.com>
2020-02-06 20:42:39 +0530

    setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -17,17 +17,18 @@ weatherForm.addEventListener('submit', (event) => {
     messageOne.textContent = 'Loading ...';
     messageTwo.textContent = '';
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            messageOne.textContent = data.error;
-            console.log(data.error);
-        } else {
-            messageOne.textContent = data.location;
-            messageTwo.textContent = data.forecast;
-            console.log(data.location);
-            console.log(data.forecast);
-        }
+    fetch('/weather?address=' + location)
+    .then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+                console.log(data.error);
+            } else {
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+                console.log(data.location);
+                console.log(data.forecast);
+            }
+        });
     });
-});
 });
